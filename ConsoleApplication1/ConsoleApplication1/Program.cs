2626cf2096e8be20e4d4db8b929f5ceae3a4d3ec
Test User <v_demidchik@tut.by>
2017-04-25 07:40:56 -0400

    0002

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,9 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine(123);
+            Console.WriteLine(123);
+            Console.WriteLine(123);
+            Console.WriteLine(123);
         }
     }
 }
