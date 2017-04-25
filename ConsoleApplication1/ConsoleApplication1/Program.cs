ed6018da9316eadfa09cd1217ce747dd68d6b273
Test User <v_demidchik@tut.by>
2017-04-25 07:51:39 -0400

    0002-4

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -15,6 +15,8 @@ namespace ConsoleApplication1
             Console.WriteLine(321);
             Console.WriteLine(321);
             Console.WriteLine(321);
+            Console.WriteLine(321); Console.WriteLine(321);
+            Console.WriteLine(321); Console.WriteLine(321);
             Console.WriteLine(321);
         }
     }
