3224569ad7e6bb6905018a4fdff988f0e38067d0
test 3 <v_demidchik@tut.by>
2017-04-25 08:14:04 -0400

    test 3 - 0003 -1

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -18,6 +18,8 @@ namespace ConsoleApplication1
             Console.WriteLine(321); Console.WriteLine(321);
             Console.WriteLine(321); Console.WriteLine(321);
             Console.WriteLine(321);
+            Console.WriteLine(321);
+            Console.WriteLine(321);
         }
     }
 }
